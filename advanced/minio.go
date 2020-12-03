8d832cef7f31756e392e6c701665252a950cd2a0
zhangzongliang <noaghzil@gamil.com>
2020-12-03 23:51:38 +0800

    Feat: go进阶学习

--- advanced/minio.go
+++ advanced/minio.go
@@ -1,40 +1,42 @@
 package advanced
 
+// https://mp.weixin.qq.com/s?__biz=MzI3MjU4Njk3Ng==&mid=2247484598&idx=1&sn=ea91646ad078518e9ffbc39994abe424&chksm=eb310539dc468c2f81bc79f2ff55e3e8e314506137746ed6bb785420d9ee49f1a289a67897d6&scene=178&cur_album_id=1368991005374234626#rd
+
 type BytePoolCap struct {
-    c    chan []byte
-    w    int
-    wcap int
+	c    chan []byte
+	w    int
+	wcap int
 }
 
 func (bp *BytePoolCap) Get() (b []byte) {
-    select {
-    case b = <-bp.c:
-    // reuse existing buffer
-    default:
-        // create new buffer
-        if bp.wcap > 0 {
-            b = make([]byte, bp.w, bp.wcap)
-        } else {
-            b = make([]byte, bp.w)
-        }
-    }
-    return
+	select {
+	case b = <-bp.c:
+	// reuse existing buffer
+	default:
+		// create new buffer
+		if bp.wcap > 0 {
+			b = make([]byte, bp.w, bp.wcap)
+		} else {
+			b = make([]byte, bp.w)
+		}
+	}
+	return
 }
 
 func (bp *BytePoolCap) Put(b []byte) {
-    select {
-    case bp.c <- b:
-        // buffer went back into pool
-    default:
-        // buffer didn't go back into pool, just discard
-    }
+	select {
+	case bp.c <- b:
+		// buffer went back into pool
+	default:
+		// buffer didn't go back into pool, just discard
+	}
 }
 
 func NewBytePoolCap(maxSize int, width int, capwidth int) (bp *BytePoolCap) {
 	// 工厂函数，用于生成*BytePoolCap
-    return &BytePoolCap{
-        c:    make(chan []byte, maxSize),
-        w:    width,
-        wcap: capwidth,
-    }
+	return &BytePoolCap{
+		c:    make(chan []byte, maxSize),
+		w:    width,
+		wcap: capwidth,
+	}
 }
--- advanced/proxy.go
+++ advanced/proxy.go
@@ -1,85 +1,86 @@
+// https://mp.weixin.qq.com/s?__biz=MzI3MjU4Njk3Ng==&mid=2247484041&idx=1&sn=1b085da6d7217006218409865e700253&chksm=eb310306dc468a10ebf6a0658a29bcd8200ee5cfcbe9fbb21e39081e5cc28af4ba00f5f6aa9a&scene=178&cur_album_id=1368991005374234626#rd
 package main
 
-import (    
+import (
 	"bytes"
-    "fmt"
-    "io"
-    "log"
-    "net"
-    "net/url"
-    "strings"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/url"
+	"strings"
 )
 
 func main2() {
-    // 设置日志格式
-    log.SetFlags(log.LstdFlags|log.Lshortfile)
-    // 监听tcp端口
-    ln, err := net.Listen("tcp", ":8081")    
-    if err != nil {
-        // 错误时，记录错误，处理defer退出
-        log.Panic(err)
-    }    
-    for {
-        // 等待客户端连接
-        client, err := ln.Accept()        
-        if err != nil {
-            log.Panic(err)
-        }        
-        // 通过goroutine来处理用户的请求
-        go handleClientRequest(client)
-    }
+	// 设置日志格式
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	// 监听tcp端口
+	ln, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		// 错误时，记录错误，处理defer退出
+		log.Panic(err)
+	}
+	for {
+		// 等待客户端连接
+		client, err := ln.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		// 通过goroutine来处理用户的请求
+		go handleClientRequest(client)
+	}
 }
 
 func handleClientRequest(client net.Conn) {
-    if client == nil {        
-        return
-    } 
-    // 关闭连接   
-    defer client.Close()    
-    var b [1024]byte
-    // 读取连接信息
-    n, err := client.Read(b[:])    
-    if err != nil {
-        log.Println(err)        
-        return
-    }    
-    var method, host, address string
-    // 返回 s 的的第一个实例的索引
-    fmt.Println(bytes.IndexByte(b[:], '\n'))
-    http_string := string(b[:bytes.IndexByte(b[:], '\n')])
-    // 字符串扫描输入
-    fmt.Sscanf(http_string, "%s%s", &method, &host)
-    hostPortURL, err := url.Parse(host)    
-    if err != nil {
-        log.Println(err)        
-        return
-    }    
-    if hostPortURL.Opaque == "443" { //https访问
-        address = hostPortURL.Scheme + ":443"
-    } else { //http访问
-        if strings.Index(hostPortURL.Host, ":") == -1 { //host不带端口，默认80
-            address = hostPortURL.Host + ":80"
-        } else {
-            address = hostPortURL.Host
-        }
-    }    
-    
-    //获得了请求的host和port，就开始拨号吧
-    server, err := net.Dial("tcp", address)    
-    if err != nil {
-        log.Println(err)        
-        return
-    }    
+	if client == nil {
+		return
+	}
+	// 关闭连接
+	defer client.Close()
+	var b [1024]byte
+	// 读取连接信息
+	n, err := client.Read(b[:])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	var method, host, address string
+	// 返回 s 的的第一个实例的索引
+	fmt.Println(bytes.IndexByte(b[:], '\n'))
+	http_string := string(b[:bytes.IndexByte(b[:], '\n')])
+	// 字符串扫描输入
+	fmt.Sscanf(http_string, "%s%s", &method, &host)
+	hostPortURL, err := url.Parse(host)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if hostPortURL.Opaque == "443" { //https访问
+		address = hostPortURL.Scheme + ":443"
+	} else { //http访问
+		if strings.Index(hostPortURL.Host, ":") == -1 { //host不带端口，默认80
+			address = hostPortURL.Host + ":80"
+		} else {
+			address = hostPortURL.Host
+		}
+	}
 
-    // ??
-    if method == "CONNECT" {
-        fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\n")
-    } else {
-        // ??
-        server.Write(b[:n])
-    }    
-    
-    //进行转发
-    go io.Copy(server, client)
-    io.Copy(client, server)
+	//获得了请求的host和port，就开始拨号吧
+	server, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// ??
+	if method == "CONNECT" {
+		fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\n")
+	} else {
+		// ??
+		server.Write(b[:n])
+	}
+
+	//进行转发
+	go io.Copy(server, client)
+	io.Copy(client, server)
 }
